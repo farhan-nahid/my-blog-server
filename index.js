15b3ba2d7a6716d867686bc3d80580da5ac34690
farhan-nahid <nahidahmed9933@gmail.com>
2021-06-26 20:49:29 +0600

    pari na

--- index.js
+++ index.js
@@ -66,19 +66,19 @@ client.connect((err) => {
                                         Register Api 
                                                                                                 */
 
-  app.post("/register", (req, res) => {
-    const registerSchema = joi.object({
-        name: Joi.string().min(3).max(30).require(),
-        email: Joi.string().email().require(),
-        password: Joi.string().pattern(new.RegExp('^[a-zA-Z0-9]{3,30}$')).require(),
-        confirm_passWord: Joi.ref('password')
-    });
-    const {error} = registerSchema.validate(req.body)
+  // app.post("/register", (req, res) => {
+  //   const registerSchema = joi.object({
+  //       name: Joi.string().min(3).max(30).require(),
+  //       email: Joi.string().email().require(),
+  //       password: Joi.string().pattern(new.RegExp('^[a-zA-Z0-9]{3,30}$')).require(),
+  //       confirm_passWord: Joi.ref('password')
+  //   });
+  //   const {error} = registerSchema.validate(req.body)
 
-    if(error){
-        res.json({})
-    }
-  });
+  //   if(error){
+  //       res.json({})
+  //   }
+  // });
   //end
 });
 
